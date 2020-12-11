a446de5c8ab9b3a0f485d90776368e60ad1b280d
gebeto <slavik.nychkalo@gmail.com>
2020-12-11 17:33:26 +0200

    asdasd

--- captchabuster/__init__.py
+++ captchabuster/__init__.py
@@ -208,9 +208,7 @@ class SessionTransferMiddleware(object):
         self.logger = logging.getLogger(self.__class__.__name__)
 
     def process_response(self, request, response, spider):
-        isin = "Sorry, we just need to make sure you're not a robot." in response.text
-        print(" >>>>> ISIN", isin)
-        if response.status in self.handle or isin:
+        if response.status in self.handle:
             cookie = self.current_cookie + 1
             self.logger.info('transferring request to new cookiejar. cookiejar={} {}'.format(cookie, request))
             meta = request.meta
